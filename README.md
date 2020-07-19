e95d8d509ce3364148e0621243ad404609b629fe
lzampieri <zampieri.leonardo98@gmail.com>
2020-07-19 23:46:40 +0200

    Cleaning

--- README.md
+++
@@ -1,2 +0,0 @@
-# fractal-plotter
-WebApp to plot fractal
--- drawer/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='drawer/index'),
-    path('upload',views.upload_file, name='drawer/upload_file')
-]
